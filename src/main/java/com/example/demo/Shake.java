81d3e59eb816bd8e6e9a276843cf406664db43fd
Anatoli Knyaz <knyaz349@mail.ru>
2022-09-29 19:03:24 +0400

    Create Shake.java

---
+++ src/main/java/com/example/demo/Shake.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+public class Shake {
+    //private TransliteTransition tt;
+
+    public Shake(Node node) {
+        //TransliteTransition(Duration.millis(70), node);
+    }
+
+}
