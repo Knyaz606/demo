68584c744a55c3d6295bb05a31b20e8a0b29d63f
Anatoli Knyaz <knyaz349@mail.ru>
2022-09-29 19:03:06 +0400

    Create Home.java

---
+++ src/main/java/com/example/demo/Home.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Home {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button Add_employee;
+
+    @FXML
+    private TextField Chief;
+
+    @FXML
+    private Button Department;
+
+    @FXML
+    private Button Exit;
+
+    @FXML
+    private Button Guide;
+
+    @FXML
+    private TextField Middle_name;
+
+    @FXML
+    private TextField Name;
+
+    @FXML
+    private Button Post;
+
+    @FXML
+    private TextField Post1;
+
+    @FXML
+    private Button Reg1111;
+
+    @FXML
+    private Button Reg11111;
+
+    @FXML
+    private Button Reg111111;
+
+    @FXML
+    private Button Reg1111111;
+
+    @FXML
+    private Button Search;
+
+    @FXML
+    private TextField Separate;
+
+    @FXML
+    private TextField Surname;
+
+    @FXML
+    private Button Workers;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
